4f752bda7a6108e9c236f573010d1794df802d2e
Tiago Pais <tiago.pais@blip.pt>
2020-01-05 22:16:49 +0000

    Update page

---
+++ precache-manifest.8ee744372b9b9de4b1e8e0142ea4026a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdffba69ceaf023ca578b6070217938a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f02af2dd7d498a84bf",
+    "url": "/static/css/main.445234b8.chunk.css"
+  },
+  {
+    "revision": "875e54692bbb367b8ada",
+    "url": "/static/js/2.54219a36.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.54219a36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6f02af2dd7d498a84bf",
+    "url": "/static/js/main.a20100ac.chunk.js"
+  },
+  {
+    "revision": "9dbbdf0bd445d9842c19",
+    "url": "/static/js/runtime-main.bf43b374.js"
+  },
+  {
+    "revision": "e1ee0b4aee9b10e92e92deca440cf4dc",
+    "url": "/static/media/background-text.e1ee0b4a.png"
+  },
+  {
+    "revision": "b3fee62bfbef7699025800e97d943022",
+    "url": "/static/media/background.b3fee62b.png"
+  }
+]);
